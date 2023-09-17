afa6cbe49eee90498fa51cd0a427ac1065c88e64
Bureau-w <jbironneau@gmail.com>
2023-09-17 12:25:15 +0200

    change path version in script

--- src/script.js
+++ src/script.js
@@ -14,11 +14,11 @@ THREE.ColorManagement.enabled = false;
  * PATHS
  */
 const dracoPath =
-  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.1/dist/draco/";
+  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.2/dist/draco/";
 const texturePath =
-  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.1/dist/textures/matcaps/12.png";
+  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.2/dist/textures/matcaps/12.png";
 const modelPath =
-  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.1/dist/models/escalier-v2.glb";
+  "https://cdn.jsdelivr.net/gh/wip-com/Metal-360@v1.2/dist/models/escalier-v2.glb";
 
 /**
  * Add CANVAS to body
